08bab81461015cc0dc4d80dec945f2a26b8528f5
n1g4g8e8r <102145810+n1g4g8e8r@users.noreply.github.com>
2022-03-22 17:40:01 +0500

    Update pxy.py

--- pxy.py
+++ pxy.py
@@ -4,3 +4,5 @@ from bs4 import BeautifulSoup
 
 url = 'https://journal.tinkoff.ru/guide/ad-sense/'  # url страницы
 r = requests.get(url)
+with open('test.html', 'w') as output_file:
+  output_file.write(r.text)
